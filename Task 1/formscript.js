ca727023cf379c20adae37a995168f38c2018265
Sirichandana789 <sirichandanareddyp@gmail.com>
2025-08-23 14:19:36 +0530

    Add files via upload

---
+++ Task 1/formscript.js
@@ -0,0 +1,4 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent page reload
+  alert("🎉 Thank you! Your registration was successful.");
+});
